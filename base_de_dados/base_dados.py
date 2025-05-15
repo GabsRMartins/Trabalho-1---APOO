c1fe18f0b825efeb8ec569033b02cf03b477100d
Elaine <elainebarbosalimaa@gmail.com>
2025-05-14 22:06:03 -0300

    feat:alterando classe banco de dados

--- base_de_dados/base_dados.py
+++ base_de_dados/base_dados.py
@@ -1,71 +1,54 @@
 import sqlite3
 
 class Base_Dados:
-    def __init__(self):#inicializa a conexão com o banco de dados
-        self.db_name = 'role_dia_db'
-        self.connection = None
+    def __init__(self, db_name:str) -> None:
+        #inicializa a conexão com o banco de dados
+        self._db_name = db_name
+        self.__connection = None
+        self.connect()
+        self.__cursor = self.__connection.cursor()
+        self.executa_script()
 
-    def connect(self):#estabelece a conexão com o banco de dados
-        try:
-            self.connection = sqlite3.connect(self.db_name)
-            print(f"Conexão com o banco '{self.db_name}' foi estabelecida com sucesso.")
-        except sqlite3.Error as e:
-            print(f"Erro ao conectar ao banco: {e}")
+    def connect(self) -> None:
+        self.__connection = sqlite3.connect(self._db_name)
 
-    def execute_script(self):#executa o arquivo .sql
-        try:
-            with open('base_de_dados/role_dia_db.sql', 'r') as file:
-                script = file.read()
-            cursor = self.connection.cursor()
-            cursor.executescript(script)
-            self.connection.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao executar o script SQL: {e}")
+    def executa_script(self) -> None:
+        #executa o arquivo .sql
+        with open('base_de_dados/role_dia_db.sql', 'r') as file:
+            script = file.read()
+        self.__cursor.executescript(script)
+        self.__connection.commit()
 
-    def add_usuario(self, nome, email, senha, tipo_usuario):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("""
-                INSERT INTO usuario (nome, email, senha, tipo_usuario) 
-                VALUES (?, ?, ?, ?)
-            """, (nome, email, senha, tipo_usuario))
-            self.connection.commit()
-        except mysql.connector.Error as e:
-            print(f"Erro ao adicionar usuário: {e}")
-
-    def add_evento(self, nome_evento, local_evento, organizadora, id_usuario):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("""
-            INSERT INTO eventos (nome_evento, local_evento, organizadora, id_usuario) 
+    def add_usuario(self, nome, email, senha, tipo_usuario) -> None:
+        self.__cursor.execute("""
+            INSERT INTO usuario (nome, email, senha, tipo_usuario) 
             VALUES (?, ?, ?, ?)
-            """, (nome_evento, local_evento, organizadora, id_usuario))
-            self.connection.commit()
-        except mysql.connector.Error as e:
-            print(f"Erro ao adicionar evento: {e}")
+        """, (nome, email, senha, tipo_usuario))
+        self.__connection.commit()
 
-    def get_usuario(self):
-        try:
-            cursor = self.connection.cursor()
-            cursor.execute("SELECT * FROM usuario ")
-            return cursor.fetchone()
-        except sqlite3.Error as e:
-            print(f"Erro ao buscar usuário com ID {user_id}: {e}")
-            return None
+    def add_evento(self, nome_evento, local_evento, organizadora, id_usuario) -> None:
+        self.__cursor.execute("""
+        INSERT INTO eventos (nome_evento, local_evento, organizadora, id_usuario) 
+        VALUES (?, ?, ?, ?)
+        """, (nome_evento, local_evento, organizadora, id_usuario))
+        self.__connection.commit()
 
-    def close(self): #encerra a conexão com o banco de dados
-        if self.connection:
-            self.connection.close()
+    def get_nome_usuario(self) -> str:
+        self.__cursor.execute("SELECT nome FROM usuario ")
+        return self.__cursor.fetchone()
 
-# # Exemplo de uso
-# db_manager = Base_Dados()
-# db_manager.connect()
-# db_manager.execute_script()
-# # db_manager.add_evento(
-# #     nome_evento='Workshop de Tecnologia',
-# #     local_evento='Auditório da UFMG',
-# #     organizadora='Elaine Santos',
-# #     id_usuario=1  # Certifique-se de que o usuário com ID 1 existe
-# # )
-# db_manager.add_usuario('Carlos', 'carlos123@example.com', 'senha_segura123', 1)
-# db_manager.close()
+    def get_nome_evento(self) -> str:
+        self.__cursor.execute("SELECT nome_evento FROM eventos ")
+        return self.__cursor.fetchone()
+
+    def __del__(self) -> None: 
+        #encerra a conexão com o banco de dados
+        if self.__connection:
+            self.__connection.close()
+
+# db = Base_Dados('role_dia_db')
+# print(type(db.get_nome_usuario()))
+# print(db.get_nome_evento())
+# # nome = db.get_nome_usuario
+
+# # print(nome)
